e6bf317e781a0d44b34b8b7cb2377350aa4f7df3
James Hanley <hanley.doth@outlook.com>
2018-11-13 14:00:33 -0800

    Add hello-world

---
+++ wp-content/themes/thecyrcletheme/footer.php
@@ -0,0 +1,9 @@
+<p>
+  Footerworks
+</p>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+<script src="../../dist/js/bootstrap.min.js"></script>
+<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
