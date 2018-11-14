272110e20ccc980e7afd07eadbebeb172f48ed99
b.palowski <brianpalowski00@gmail.com>
2018-11-13 17:48:37 -0800

    Footer

--- wp-content/themes/thecyrcletheme/footer.php
+++ wp-content/themes/thecyrcletheme/footer.php
@@ -3,7 +3,56 @@
 </div><!-- /.container -->
 
 <footer class="blog-footer">
-    <p>Did it brian</p>
+  <div class="color-background">
+<div class="icons">
+<a href="#" class="fa fa-facebook"></a><a href="#" class="fa fa-twitter"></a>
+<a href="#" class="fa fa-linkedin"></a><a href="#" class="fa fa-youtube"></a>
+</div>
+<div class="flex-container">
+
+<!-- <div class="icons">
+<a href="#" class="fa fa-facebook"></a><a href="#" class="fa fa-twitter"></a>
+<a href="#" class="fa fa-linkedin"></a><a href="#" class="fa fa-youtube"></a>
+</div> -->
+
+
+  <div class="section section1">
+  <h1>Products</h1>
+  <p><a href="https://www.cyrclephone.com/">CyrclePhone</a></p>
+    <p><a href="https://www.kickstarter.com/projects/1662021259/make-your-own-cyrcle-phone">Kickstarter</a></p>
+  </div>
+
+  <div class="section section2">
+    <h1>Company</h1>
+    <p><a href="https://dtoor.com">dTOOR's Website</a></p>
+    <p><a href="https://dtoor.com/people/">Team</a></p>
+    <p><a href="https://angel.co/dtoor-inc-spc/jobs">Careers</a></p>
+    <p><a href="https://dtoor.com/contact/">Contact Us</a></p>
+  </div>
+
+  <div class="section section3">
+    <h1>News</h1>
+    <p><a href="https://dtoor.com/press/">Press</a></p>
+    <p><a href="https://onedrive.live.com/view.aspx?resid=85DF849C68597C77!76528&ithint=file%2cdocx&app=Word&authkey=!AEZjtE0g8J2ek_k">Press Kit</a></p>
+    <p><a href="https://dtoor.com/blog/">Blog</a></p>
+      <p><a href="https://www.youtube.com/channel/UCshjB0L-2t11wnpCJNcJqJA">Videos</a></p>
+  </div>
+</div>
+
+
+
+
+
+<div class="bottom-privacy">
+<div class="sec ">
+      <h1><a href="http://dtoor.com"><img src="./../../assets/images/t.png" /></a></h1>
+  <p> Copyright dTOOR 2018 <a href="https://dtoor.com/privacy-policy"> Privacy Policy </a> |
+  <a href="https://dtoor.com/social-purpose-statement"> Annual Report </a></p>
+</div>
+</div>
+
+</div>
+
 </footer>
 <?php wp_footer(); ?>
 </body>
