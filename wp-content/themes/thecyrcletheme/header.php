d2c1eea2541c7f421526e56d36b5b12b28992d09
b.palowski <brianpalowski00@gmail.com>
2018-11-14 13:01:14 -0800

    Test index content

--- wp-content/themes/thecyrcletheme/header.php
+++ wp-content/themes/thecyrcletheme/header.php
@@ -4,6 +4,7 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./node_modules/sal.js/dist/sal.css">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
