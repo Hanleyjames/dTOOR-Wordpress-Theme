4619675d9c30ac006728445dc1d84c843d9bc5a5
b.palowski <brianpalowski00@gmail.com>
2018-11-14 13:50:05 -0800

    footer icon dtoor

--- wp-content/themes/thecyrcletheme/index.php
+++ wp-content/themes/thecyrcletheme/index.php
@@ -1,13 +1,10 @@
-<?php get_header(); ?>
 <div class="t"></div>
+<?php get_header(); ?>
+
 
 <div class="flex-container-content">
 
 <div id="my-clouds">
-
-    <marquee behavior="scroll" direction="left" scrollamount="5">
-      <i class="fab fa-accessible-icon" style="color: black"></i>
-    </marquee>
     <marquee behavior="scroll" direction="right" scrollamount="14">
         <i class="fa fa-cloud" style="color:white;font-size:40px"></i>
     </marquee>
@@ -32,14 +29,11 @@
   <p>Our Cyrcle Phone prototype had a successful <a href="https://www.kickstarter.com/projects/1662021259/make-your-own-cyrcle-phone"><strong>Kickstarter campaign</strong></a>. We met our kickstarter goal and since then gaining incredible traction. We believe our unique smart phone design and concepts breathes fresh air in an otherwise drab market.</p>
 </div>
 
-  <div class="section num3">
+  <div class="section num2">
   <h2>Unique phones, unique ideas.</h2>
   <p>The Cyrcle phone isn't your ordinary mobile device. We're building a unique experience missed by other kickstarter projects. We feel the need and want for a unique phone design has long since been neglected -- and we're answering the call! We look forward to raising the bar as the best curved phone on the market.</p>
 </div>
 <div id="my-clouds">
-    <marquee behavior="scroll" direction="right" scrollamount="15">
-        <i class="fa fa-cloud" style="color:white;font-size:40px"></i>
-    </marquee>
     <marquee behavior="scroll" direction="left" scrollamount="10">
         <i class="fa fa-cloud" style="color:white;font-size:60px"></i>
     </marquee>
