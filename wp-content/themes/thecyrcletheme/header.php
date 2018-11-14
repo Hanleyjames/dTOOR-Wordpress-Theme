7c41b97e7363d68e4dc4f61345d2328cd6c7ab8a
b.palowski <brianpalowski00@gmail.com>
2018-11-14 12:53:22 -0800

    Removed nav

--- wp-content/themes/thecyrcletheme/header.php
+++ wp-content/themes/thecyrcletheme/header.php
@@ -13,14 +13,6 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-  <div class="container">
-    <div class="nav-menu">
-      <img class="nav-toggle nav-ring" id="imgs" src="<?php echo get_template_directory_uri(); ?>/library/images/navlogo.png" tabindex="0" /><a id="logo" class="nav-ring" href="#" tabindex="0"></a>
-      <a class="nav-toggle nav-ring" tabindex="1"></a><a href="https://dtoor.com/social-purpose-statement" id="pink" class="nav-ring" tabindex="0"><span>About Us</span></a>
-      <a class="nav-toggle nav-ring" tabindex="2"></a><a href="https://dtoor.com/blog" class="nav-ring" id="yellow" tabindex="0"><span>Blog</span></a>
-      <a class="nav-toggle nav-ring" tabindex="3"></a><a href="https://dtoor.com" class="nav-ring" id="blue" tabindex="0"><span>Our Company</span></a>
-    </div>
-  </div>
   <div class="landing-image">
   </div>
   <div class="project-introduction">
