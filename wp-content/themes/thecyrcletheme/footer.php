af54e82719795390b9d4ba6f2dc608d1322db4a7
b.palowski <brianpalowski00@gmail.com>
2018-11-14 14:36:01 -0800

    index fix

--- wp-content/themes/thecyrcletheme/footer.php
+++ wp-content/themes/thecyrcletheme/footer.php
@@ -1,9 +1,9 @@
 <footer class="blog-footer">
 <div class="color-background">
-<div class="icons">
+<!-- <div class="icons">
 <a href="#" class="fa fa-facebook"></a><a href="#" class="fa fa-twitter"></a>
 <a href="#" class="fa fa-linkedin"></a><a href="#" class="fa fa-youtube"></a>
-</div>
+</div> -->
 <div class="flex-container-foot">
 
 
@@ -37,12 +37,22 @@
 
 
 <div class="bottom-privacy">
-<div class="sec ">
+<!-- <div class="sec "> -->
+  <div class="row">
 
+<div class="col-md-6">
   <h1><a href="http://dtoor.com"><img src="<?php echo get_template_directory_uri(); ?>/library/images/t.png" id="imgFooter" /></a></h1>
 
   <p> Copyright dTOOR 2018 <a href="https://dtoor.com/privacy-policy"> Privacy Policy </a> |
   <a href="https://dtoor.com/social-purpose-statement"> Annual Report </a></p>
+</div>
+
+
+<div class="col-md-6">
+<div class="icons">
+<a href="#" class="fa fa-facebook"></a><a href="#" class="fa fa-twitter"></a>
+<a href="#" class="fa fa-linkedin"></a><a href="#" class="fa fa-youtube"></a>
+</div>
 </div>
 </div>
 
