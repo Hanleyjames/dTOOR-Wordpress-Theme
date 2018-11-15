cf76e566533088325d3ef6e3c8c472d736a11db4
b.palowski <brianpalowski00@gmail.com>
2018-11-14 17:23:20 -0800

    index fix

--- wp-content/themes/thecyrcletheme/footer.php
+++ wp-content/themes/thecyrcletheme/footer.php
@@ -6,7 +6,7 @@
 </div> -->
 <div class="foot">
 
-  <div class="icon">
+  <div class="row">
   <a href="#" class="fa fa-facebook"></a><a href="#" class="fa fa-twitter"></a>
   <a href="#" class="fa fa-linkedin"></a><a href="#" class="fa fa-youtube"></a>
   </div>
