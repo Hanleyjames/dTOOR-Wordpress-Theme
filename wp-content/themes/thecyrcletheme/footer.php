f7136b1d641395509104a516ec67c2b59d24b8d9
James Hanley <hanley.doth@outlook.com>
2018-11-15 15:22:55 -0800

    Cleaned up footer indentation and front-page indentation

--- wp-content/themes/thecyrcletheme/footer.php
+++ wp-content/themes/thecyrcletheme/footer.php
@@ -1,60 +1,60 @@
 
-<div class="foot">
+  <div class="foot">
 
 
-<div class="flex-container-foot">
-  <div class="section section1">
-  <h2>Products</h2>
-  <p><a href="https://www.cyrclephone.com/">CyrclePhone</a></p>
-    <p><a href="https://www.kickstarter.com/projects/1662021259/make-your-own-cyrcle-phone">Kickstarter</a></p>
-  </div>
+    <div class="flex-container-foot">
+      <div class="section section1">
+        <h2>Products</h2>
+        <p><a href="https://www.cyrclephone.com/">CyrclePhone</a></p>
+        <p><a href="https://www.kickstarter.com/projects/1662021259/make-your-own-cyrcle-phone">Kickstarter</a></p>
+      </div>
 
-  <div class="section section2">
-    <h2>Company</h2>
-    <p><a href="https://dtoor.com">dTOOR's Website</a></p>
-    <p><a href="https://dtoor.com/people/">Team</a></p>
-    <p><a href="https://angel.co/dtoor-inc-spc/jobs">Careers</a></p>
-    <p><a href="https://dtoor.com/contact/">Contact Us</a></p>
-  </div>
+      <div class="section section2">
+        <h2>Company</h2>
+        <p><a href="https://dtoor.com">dTOOR's Website</a></p>
+        <p><a href="https://dtoor.com/people/">Team</a></p>
+        <p><a href="https://angel.co/dtoor-inc-spc/jobs">Careers</a></p>
+        <p><a href="https://dtoor.com/contact/">Contact Us</a></p>
+      </div>
 
-  <div class="section section3">
-    <h2>News</h2>
-    <p><a href="https://dtoor.com/press/">Press</a></p>
-    <p><a href="https://onedrive.live.com/view.aspx?resid=85DF849C68597C77!76528&ithint=file%2cdocx&app=Word&authkey=!AEZjtE0g8J2ek_k">Press Kit</a></p>
-    <p><a href="https://dtoor.com/blog/">Blog</a></p>
-      <p><a href="https://www.youtube.com/channel/UCshjB0L-2t11wnpCJNcJqJA">Videos</a></p>
-  </div>
+      <div class="section section3">
+        <h2>News</h2>
+        <p><a href="https://dtoor.com/press/">Press</a></p>
+        <p><a href="https://onedrive.live.com/view.aspx?resid=85DF849C68597C77!76528&ithint=file%2cdocx&app=Word&authkey=!AEZjtE0g8J2ek_k">Press Kit</a></p>
+        <p><a href="https://dtoor.com/blog/">Blog</a></p>
+        <p><a href="https://www.youtube.com/channel/UCshjB0L-2t11wnpCJNcJqJA">Videos</a></p>
+      </div>
 
-</div>
-
-
+    </div>
 
 
 
-<footer class="foot-content-below">
 
 
-  <div class="row">
-    <div class="col-md-6">
-  <h1><a href="http://dtoor.com"><img src="<?php echo get_template_directory_uri(); ?>/library/images/t.png" id="imgFooter" /></a></h1>
-
-  <p> Copyright dTOOR 2018 <a href="https://dtoor.com/privacy-policy"> Privacy Policy </a> |
-  <a href="https://dtoor.com/social-purpose-statement"> Annual Report </a></p>
-</div>
-
-  <div class="col-md-6">
-  <div class="icon">
-  <a href="#" class="fa fa-facebook"></a>
-  <a href="#" class="fa fa-twitter"></a>
-  <a href="#" class="fa fa-linkedin"></a>
-  <a href="#" class="fa fa-youtube"></a>
-  </div>
-</div>
-</div>
+    <footer class="foot-content-below">
 
 
-</footer>
-</div>
-<?php wp_footer(); ?>
-</body>
+      <div class="row">
+        <div class="col-md-6">
+          <h1><a href="http://dtoor.com"><img src="<?php echo get_template_directory_uri(); ?>/library/images/t.png" id="imgFooter" /></a></h1>
+
+          <p> Copyright dTOOR 2018 <a href="https://dtoor.com/privacy-policy"> Privacy Policy </a> |
+            <a href="https://dtoor.com/social-purpose-statement"> Annual Report </a></p>
+          </div>
+
+          <div class="col-md-6">
+            <div class="icon">
+              <a href="#" class="fa fa-facebook"></a>
+              <a href="#" class="fa fa-twitter"></a>
+              <a href="#" class="fa fa-linkedin"></a>
+              <a href="#" class="fa fa-youtube"></a>
+            </div>
+          </div>
+        </div>
+
+
+      </footer>
+    </div>
+    <?php wp_footer(); ?>
+  </body>
 </html>
--- wp-content/themes/thecyrcletheme/front-page.php
+++ wp-content/themes/thecyrcletheme/front-page.php
@@ -7,49 +7,52 @@
 <div class="t"></div>
 <div class="flex-container-content">
 
-<div id="my-clouds">
+  <div id="my-clouds">
     <marquee behavior="scroll" direction="right" scrollamount="14">
-        <i class="fa fa-cloud" style="color:white;font-size:40px"></i>
+      <i class="fa fa-cloud" style="color:white;font-size:40px"></i>
     </marquee>
 
     <marquee behavior="scroll" direction="right" scrollamount="5">
-        <i class="fa fa-cloud" style="color:white;font-size:90px"></i>
+      <i class="fa fa-cloud" style="color:white;font-size:90px"></i>
     </marquee>
     <marquee behavior="scroll" direction="left" scrollamount="5">
-        <i class="fa fa-cloud" style="color:white;font-size:10px"></i>
+      <i class="fa fa-cloud" style="color:white;font-size:10px"></i>
     </marquee>
-</div>
-<div class="container">
+  </div>
+  <div class="container">
     <h1>The Cyrcle Phone</h1>
 
-  <div class="section num1">
-  <h2>Unique phones for non-rectangular people</h2>
-  <p>Cyrcle Phone is a unique phones company with a novel aesthetic design. Unlike other designer phones which repeat the same design, we had a mission to make mobile devices for women. The release builds were a huge success, crossing the boundaries of gender and age. Our non-traditional phones feature a non-rectangular, round design. We believe this is perfect for those who don't consider themselves non-rectangular.</p>
+    <div class="section num1">
+      <h2>Unique phones for non-rectangular people</h2>
+      <p>Cyrcle Phone is a unique phones company with a novel aesthetic design. Unlike other designer phones which repeat the same design, we had a mission to make mobile devices for women. The release builds were a huge success, crossing the boundaries of gender and age. Our non-traditional phones feature a non-rectangular, round design. We believe this is perfect for those who don't consider themselves non-rectangular.</p>
 
 
 
-  <h2>Our diy smartphone journey</h2>
-  <p>Our Cyrcle Phone prototype had a successful <a href="https://www.kickstarter.com/projects/1662021259/make-your-own-cyrcle-phone"><strong>Kickstarter campaign</strong></a>. We met our kickstarter goal and since then gaining incredible traction. We believe our unique smart phone design and concepts breathes fresh air in an otherwise drab market.</p>
-</div>
+      <h2>Our diy smartphone journey</h2>
+      <p>Our Cyrcle Phone prototype had a successful <a href="https://www.kickstarter.com/projects/1662021259/make-your-own-cyrcle-phone"><strong>Kickstarter campaign</strong></a>. We met our kickstarter goal and since then gaining incredible traction. We believe our unique smart phone design and concepts breathes fresh air in an otherwise drab market.</p>
+    </div>
 
-  <div class="section num2">
-  <h2>Unique phones, unique ideas.</h2>
-  <p>The Cyrcle phone isn't your ordinary mobile device. We're building a unique experience missed by other kickstarter projects. We feel the need and want for a unique phone design has long since been neglected -- and we're answering the call! We look forward to raising the bar as the best curved phone on the market.</p>
-</div>
-</div>
-<div id="my-clouds">
+    <div class="section num2">
+      <h2>Unique phones, unique ideas.</h2>
+      <p>The Cyrcle phone isn't your ordinary mobile device. We're building a unique experience missed by other kickstarter projects. We feel the need and want for a unique phone design has long since been neglected -- and we're answering the call! We look forward to raising the bar as the best curved phone on the market.</p>
+    </div>
+  </div>
+  <div id="my-clouds">
     <marquee behavior="scroll" direction="left" scrollamount="10">
-        <i class="fa fa-cloud" style="color:white;font-size:60px"></i>
+      <i class="fa fa-cloud" style="color:white;font-size:60px"></i>
     </marquee>
     <marquee behavior="scroll" direction="right" scrollamount="5">
-        <i class="fa fa-cloud" style="color:white;font-size:90px"></i>
+      <i class="fa fa-cloud" style="color:white;font-size:90px"></i>
     </marquee>
     <marquee behavior="scroll" direction="left" scrollamount="5">
-        <i class="fa fa-cloud" style="color:white;font-size:10px"></i>
+      <i class="fa fa-cloud" style="color:white;font-size:10px"></i>
     </marquee>
-</div>
+  </div>
 
 </div>
 
 <div class="phone"></div>
+<div class="FAQ">
+  
+</div>
 <?php get_footer(); ?>
