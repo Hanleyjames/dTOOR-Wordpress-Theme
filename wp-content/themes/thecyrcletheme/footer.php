a8ea2788dd4f2f2533a8bc537761a2da980de423
b.palowski <brianpalowski00@gmail.com>
2018-11-14 16:10:35 -0800

    index fix

--- wp-content/themes/thecyrcletheme/footer.php
+++ wp-content/themes/thecyrcletheme/footer.php
@@ -42,7 +42,7 @@
 <footer>
   <!-- <div class="row">
   <div class="col-md-6"> -->
-     <div class="float-left">
+     <div class="test">
   <h1><a href="http://dtoor.com"><img src="<?php echo get_template_directory_uri(); ?>/library/images/t.png" id="imgFooter" /></a></h1>
 
   <p> Copyright dTOOR 2018 <a href="https://dtoor.com/privacy-policy"> Privacy Policy </a> |
