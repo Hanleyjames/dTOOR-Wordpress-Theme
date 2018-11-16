ed9a32d03483fef2db645bd4beac1aae81212aed
b.palowski <brianpalowski00@gmail.com>
2018-11-16 11:11:08 -0800

    update form

--- wp-content/themes/thecyrcletheme/front-page.php
+++ wp-content/themes/thecyrcletheme/front-page.php
@@ -3,6 +3,7 @@
 
 <div class="landing-image"></div>
 
+
 <div class="less-invasive-mailchimp col-md-12">
   <!-- Begin Mailchimp Signup Form -->
 
@@ -25,37 +26,9 @@
   </div>
   <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
   <!--End mc_embed_signup-->
-</div>  <div class="less-invasive-mailchimp col-md-12">
-    <!-- Begin Mailchimp Signup Form -->
-
-    <div id="mc_embed_signup front-page-mc">
-    <form action="https://thecyrclephone.us12.list-manage.com/subscribe/post?u=01a458396e37205a5745fbfec&amp;id=7b4ccb81be" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-       <div id="mc_embed_signup_scroll">
-    <div class="mc-field-group">
-        <label for="mce-EMAIL" id="email-label">Get Updated
-    </label>
-        <input id="email-input" type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email Required" required>
-    </div>
-        <div id="mce-responses" class="clear">
-            <div class="response" id="mce-error-response" style="display:none"></div>
-            <div class="response" id="mce-success-response" style="display:none"></div>
-        </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-       <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_01a458396e37205a5745fbfec_7b4ccb81be" tabindex="-1" value=""></div>
-       <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-       </div>
-    </form>
-    </div>
-    <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
-    <!--End mc_embed_signup-->
-  </div>
-
-
-
-
-
+</div>
 
 <div class="flex-container-content">
-
 
   <div id="my-clouds" class="col-md-12">
     <marquee behavior="scroll" direction="right" scrollamount="14">
@@ -108,7 +81,6 @@
 <div class="FAQ">
   <h2>FAQ</h2>
   <br />
-
   <div id="question-answer">
     <div class="question">
       <h3>Why are there no images of your new phone model?</h3>
@@ -144,7 +116,7 @@
       <h3>How can I find out more about the CyrclePhone 4G?</h3>
     </div>
     <div class="answer">
-      <p>Sign up for our updates! It's the best way to know about our current development. Otherwise, you can check out our Cyrcle Phone 2G Kickstarter and Social Purpose Statement!</p>
+      <p>Sign up for our updates! It's the best way to know about our current development. Otherwise, you can check out our (Old)Kickstarter and Social Purpose Statement!</p>
     </div>
   </div>
 </div>
