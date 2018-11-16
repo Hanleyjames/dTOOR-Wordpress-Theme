73709909a7ca36b019a694515dfa21ac75c0276b
James Hanley <hanley.doth@outlook.com>
2018-11-16 08:43:46 -0800

    Add title

--- wp-content/themes/thecyrcletheme/header.php
+++ wp-content/themes/thecyrcletheme/header.php
@@ -2,6 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <title>The Cyrcle Phone</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
