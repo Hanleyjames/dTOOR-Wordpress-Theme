81b37ec599be7e6d7f8eb2e0afd77d3423e92374
James Hanley <hanley.doth@outlook.com>
2018-11-16 11:18:08 -0800

    swag

--- wp-content/themes/thecyrcletheme/front-page.php
+++ wp-content/themes/thecyrcletheme/front-page.php
@@ -4,59 +4,27 @@
 <div class="landing-image"></div>
 
 <div class="less-invasive-mailchimp col-md-12">
-  <!-- Begin Mailchimp Signup Form -->
-
-  <div id="mc_embed_signup front-page-mc">
-  <form action="https://thecyrclephone.us12.list-manage.com/subscribe/post?u=01a458396e37205a5745fbfec&amp;id=7b4ccb81be" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-     <div id="mc_embed_signup_scroll">
-  <div class="mc-field-group">
-      <label for="mce-EMAIL" id="email-label">Get Updated
-  </label>
-      <input id="email-input" type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email Required" required>
-  </div>
-      <div id="mce-responses" class="clear">
-          <div class="response" id="mce-error-response" style="display:none"></div>
-          <div class="response" id="mce-success-response" style="display:none"></div>
-      </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-     <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_01a458396e37205a5745fbfec_7b4ccb81be" tabindex="-1" value=""></div>
-     <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-     </div>
-  </form>
-  </div>
-  <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
-  <!--End mc_embed_signup-->
-</div>  <div class="less-invasive-mailchimp col-md-12">
-    <!-- Begin Mailchimp Signup Form -->
-
+  <div class="flex-container-content">
     <div id="mc_embed_signup front-page-mc">
-    <form action="https://thecyrclephone.us12.list-manage.com/subscribe/post?u=01a458396e37205a5745fbfec&amp;id=7b4ccb81be" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-       <div id="mc_embed_signup_scroll">
-    <div class="mc-field-group">
-        <label for="mce-EMAIL" id="email-label">Get Updated
-    </label>
-        <input id="email-input" type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email Required" required>
-    </div>
-        <div id="mce-responses" class="clear">
+      <form action="https://thecyrclephone.us12.list-manage.com/subscribe/post?u=01a458396e37205a5745fbfec&amp;id=7b4ccb81be" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+        <div id="mc_embed_signup_scroll">
+          <div class="mc-field-group">
+            <label for="mce-EMAIL" id="email-label">Get Updated
+            </label>
+            <input id="email-input" type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Email Required" required>
+          </div>
+          <div id="mce-responses" class="clear">
             <div class="response" id="mce-error-response" style="display:none"></div>
             <div class="response" id="mce-success-response" style="display:none"></div>
-        </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-       <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_01a458396e37205a5745fbfec_7b4ccb81be" tabindex="-1" value=""></div>
-       <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-       </div>
-    </form>
+          </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+          <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_01a458396e37205a5745fbfec_7b4ccb81be" tabindex="-1" value=""></div>
+          <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+        </div>
+      </form>
     </div>
     <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
     <!--End mc_embed_signup-->
   </div>
-
-
-
-
-
-
-<div class="flex-container-content">
-
-
   <div id="my-clouds" class="col-md-12">
     <marquee behavior="scroll" direction="right" scrollamount="14">
       <i class="fa fa-cloud" style="color:white;font-size:40px"></i>
@@ -91,16 +59,16 @@
   </div>
   <div id="my-clouds col-md-12
   ">
-    <marquee behavior="scroll" direction="left" scrollamount="10">
-      <i class="fa fa-cloud" style="color:white;font-size:60px"></i>
-    </marquee>
-    <marquee behavior="scroll" direction="right" scrollamount="5">
-      <i class="fa fa-cloud" style="color:white;font-size:90px"></i>
-    </marquee>
-    <marquee behavior="scroll" direction="left" scrollamount="5">
-      <i class="fa fa-cloud" style="color:white;font-size:10px"></i>
-    </marquee>
-  </div>
+  <marquee behavior="scroll" direction="left" scrollamount="10">
+    <i class="fa fa-cloud" style="color:white;font-size:60px"></i>
+  </marquee>
+  <marquee behavior="scroll" direction="right" scrollamount="5">
+    <i class="fa fa-cloud" style="color:white;font-size:90px"></i>
+  </marquee>
+  <marquee behavior="scroll" direction="left" scrollamount="5">
+    <i class="fa fa-cloud" style="color:white;font-size:10px"></i>
+  </marquee>
+</div>
 
 </div>
 
