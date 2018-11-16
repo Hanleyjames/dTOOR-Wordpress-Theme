d6ef29504cab4e5494e5315f42745bd0addcd68a
James Hanley <hanley.doth@outlook.com>
2018-11-16 05:46:07 -0800

    Clean up html in email

--- wp-content/themes/thecyrcletheme/footer.php
+++ wp-content/themes/thecyrcletheme/footer.php
@@ -1,14 +1,10 @@
-
   <div class="foot">
-
-
     <div class="flex-container-foot">
       <div class="section section1">
         <h2>Products</h2>
         <p><a href="https://www.thecyrclephone.com/">CyrclePhone</a></p>
         <p><a href="https://www.kickstarter.com/projects/1662021259/make-your-own-cyrcle-phone">Kickstarter</a></p>
       </div>
-
       <div class="section section2">
         <h2>Company</h2>
         <p><a href="https://dtoor.com">dTOOR's Website</a></p>
@@ -16,7 +12,6 @@
         <p><a href="https://angel.co/dtoor-inc-spc/jobs">Careers</a></p>
         <p><a href="https://dtoor.com/contact/">Contact Us</a></p>
       </div>
-
       <div class="section section3">
         <h2>News</h2>
         <p><a href="https://dtoor.com/press/">Press</a></p>
@@ -24,24 +19,14 @@
         <p><a href="https://dtoor.com/blog/">Blog</a></p>
         <p><a href="https://www.youtube.com/channel/UCshjB0L-2t11wnpCJNcJqJA">Videos</a></p>
       </div>
-
     </div>
-
-
-
-
-
     <footer class="foot-content-below">
-
-
       <div class="row">
         <div class="col-md-6">
           <h1><a href="http://dtoor.com"><img src="<?php echo get_template_directory_uri(); ?>/library/images/t.png" id="imgFooter" /></a></h1>
-
           <p> Copyright dTOOR 2018 <a href="https://dtoor.com/privacy-policy"> Privacy Policy </a> |
             <a href="https://dtoor.com/social-purpose-statement"> Annual Report </a></p>
           </div>
-
           <div class="col-md-4">
             <div class="icon">
               <a href="#" class="fa fa-facebook"></a>
@@ -64,9 +49,9 @@
                 <h2>Subscribe to our mailing list</h2>
             <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
             <div class="mc-field-group">
-                <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+                <label for="mce-EMAIL" id="email-label">Email Address  <span class="asterisk">*</span>
             </label>
-                <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                <input id="email-input" type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
             </div>
                 <div id="mce-responses" class="clear">
                     <div class="response" id="mce-error-response" style="display:none"></div>
@@ -81,8 +66,6 @@
             <!--End mc_embed_signup-->
           </div>
         </div>
-
-
       </footer>
     </div>
     <?php wp_footer(); ?>
