9682aa8f9efdbfb5b962a14bec55783f76faafc6
James Hanley <hanley.doth@outlook.com>
2018-11-15 20:24:18 -0800

    ??

--- wp-content/themes/thecyrcletheme/404.php
+++ wp-content/themes/thecyrcletheme/404.php
@@ -2,14 +2,14 @@
 <div class="404">
   <div id="my-storm">
     <marquee behavior="scroll" direction="right" scrollamount="14">
-      <i class="fas fa-cloud-showers-heavy" style="color:grey;font-size:10px"></i>
+      <i class="fa fa-cloud-showers-heavy" style="color:grey;font-size:10px"></i>
     </marquee>
 
     <marquee behavior="scroll" direction="right" scrollamount="5">
-      <i class="fas fa-cloud-showers-heavy" style="color:grey;font-size:10px"></i>
+      <i class="fa fa-cloud-showers-heavy" style="color:grey;font-size:10px"></i>
     </marquee>
     <marquee behavior="scroll" direction="left" scrollamount="5">
-      <i class="fas fa-cloud-showers-heavy" style="color:grey;font-size:10px"></i>
+      <i class="fa fa-cloud-showers-heavy" style="color:grey;font-size:10px"></i>
     </marquee>
   </div>
   <h2>Something seems to have gone wrong</h2>
