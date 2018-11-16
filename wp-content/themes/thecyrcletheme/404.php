0ef1cfacb027dc6f1245bc7e768d59ef7104653e
James Hanley <hanley.doth@outlook.com>
2018-11-16 05:09:00 -0800

    tried diferent fa for 404

--- wp-content/themes/thecyrcletheme/404.php
+++ wp-content/themes/thecyrcletheme/404.php
@@ -2,14 +2,14 @@
 <div class="404">
   <div id="my-storm">
     <marquee behavior="scroll" direction="right" scrollamount="14">
-      <i class="fa fa-cloud-showers-heavy" style="color:grey;font-size:10px"></i>
+      <i class="fas fa-cloud-rain" style="color:grey;font-size:12px"></i>
     </marquee>
 
-    <marquee behavior="scroll" direction="right" scrollamount="5">
-      <i class="fa fa-cloud-showers-heavy" style="color:grey;font-size:10px"></i>
+    <marquee behavior="scroll" direction="right" scrollamount="7">
+      <i class="fas fa-cloud-rain" style="color:grey;font-size:10px"></i>
     </marquee>
-    <marquee behavior="scroll" direction="left" scrollamount="5">
-      <i class="fa fa-cloud-showers-heavy" style="color:grey;font-size:10px"></i>
+    <marquee behavior="scroll" direction="right" scrollamount="7">
+      <i class="fas fa-cloud-rain" style="color:grey;font-size:14px"></i>
     </marquee>
   </div>
   <h2>Something seems to have gone wrong</h2>
