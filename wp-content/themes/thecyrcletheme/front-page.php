11f4a5e9d6f60f0bfe363b6b3bbf25a4a10c61d1
James Hanley <hanley.doth@outlook.com>
2018-11-16 11:55:17 -0800

    Swag

--- wp-content/themes/thecyrcletheme/front-page.php
+++ wp-content/themes/thecyrcletheme/front-page.php
@@ -44,18 +44,18 @@
     <h1>The Cyrcle Phone 4G</h1>
 
     <div class="section num1">
-      <h2>Unique phones for non-rectangular people</h2>
-      <p>Cyrcle Phone is a unique phones company with a novel aesthetic design. Unlike other designer phones which repeat the same design, we had a mission to make non-rectangular mobile devices. The release builds were a huge success, crossing the boundaries of gender and age. We widened our scope and strive for inclusitivity. Our non-traditional phones feature a non-rectangular, round design. We believe this is perfect for those who don't consider themselves non-rectangular.</p>
+      <h2>Unique Phones for Non-Rectangular People</h2>
+      <p>The Cyrcle Phone is a unique phone with a clever shape. Unlike other designer phones which repeat the same configuration year after year, we are on a mission to make Non-Rectangular Phones for Non-Rectangular People. Through our products we cross boundaries of gender and age, and strive for diversity and inclusion. Our non-traditional phones feature a non-rectangular design. We believe these products are perfect for those who like to explore beyond the status qu</p>
 
 
 
-      <h2>Our diy smartphone journey</h2>
-      <p>Our Cyrcle Phone 2G prototype had a successful <a href="https://www.kickstarter.com/projects/1662021259/make-your-own-cyrcle-phone"><strong>Kickstarter campaign</strong></a>. We met our kickstarter goal and since then gaining incredible traction. We believe our unique smart phone design and concepts breathes fresh air in an otherwise drab market.</p>
+      <h2>Our Smartphone Journey</h2>
+      <p>Our Cyrcle Phone 2G prototype had a successful Kickstarter campaign. We met our Kickstarter goal, delivered all phones in less than 2 months(!), and have been gaining incredible traction ever since. We believe our unique smart phone design and concepts breathe fresh air and innovation into an otherwise boring digital landscape</p>
     </div>
 
     <div class="section num2">
-      <h2>Unique phones, unique ideas.</h2>
-      <p>The Cyrcle Phone 4G isn't your ordinary mobile device. We're building a unique experience missed by other kickstarter projects. We feel the need and want for a unique phone design has long since been neglected -- and we're answering the call! We look forward to raising the bar as the best curved phone on the market.</p>
+      <h2>Unique Phones, Unique Ideas.</h2>
+      <p>The Cyrcle Phone 4G isn't your ordinary mobile device. We're building an experience missed by other projects. We feel the desire for a unique phone design has long since been neglected -- and we're answering the call! We look forward to raising the bar as the best curved phone on the market. </p>
     </div>
   </div>
   <div id="my-clouds col-md-12
@@ -82,37 +82,37 @@
       <h3>Why are there no images of your new phone model?</h3>
     </div>
     <div class="answer">
-      <p>We are saving images of the phone for a week before the launche since we would like to keep our design uniquely our own. However, if you're interested in learning more you can explore our youtube channel or sign up for our mailing list.</p>
+      <p>We are saving images of the phone for a week before launch since we would like to keep the design uniquely our own. However, if you're interested in learning more until then you can explore our multiple YouTube channels or sign up for our mailing list.</p>
     </div>
     <div class="question">
-      <h3>When is the CyrclePhone 4G being launched?</h3>
+      <h3>When is the Cyrcle Phone 4G being launched?</h3>
     </div>
     <div class="answer">
-      <p>2018</p>
+      <p>2019</p>
     </div>
     <div class="question">
-      <h3>What does 'non-rectangular' mean?</h3>
+      <h3>What Does 'Non-Rectangular' Mean?</h3>
     </div>
     <div class="answer">
       <p>Anything that goes against convention(legally). Be different. Think outside of the rectangle.</p>
     </div>
     <div class="question">
-      <h3>So, why a circle?</h3>
+      <h3>So, Why a Circle?</h3>
     </div>
     <div class="answer">
-      <p>Rectangles, unlike circles, are man-made shapes. They don't occur naturally. Circles are ubiquitous in nature. They're cool. They're different. Plus they fit in your pocket with ease.</p>
+      <p>Rectangles, unlike circles, are man-made shapes. They don't occur naturally. Circles are ubiquitous in nature. They're cool. They're different. Plus, they fit in your hand and pocket with ease.</p>
     </div>
     <div class="question">
-      <h3>What are the specifications for the CyrclePhone 4G?</h3>
+      <h3>What are The Cyrcle Phone 4G Specifications?</h3>
     </div>
     <div class="answer">
-      <p>They are LTE 4g Android phones, but for the same reasons mentioned in the first answer, we cannot disclose any more details about the phone at this time.</p>
+      <p>The Cyrcle Phone 4G is an LTE Android smartphone with a large round display over 3” in diameter. Unlike the 2G model, the 4G model does not have a hinge. Rather, the 4G model is open-faced like a traditional smartphone, simply with a round display. However, for the same reasons mentioned in the first answer, we cannot disclose any more details about the phone at this time. </p>
     </div>
     <div class="question">
-      <h3>How can I find out more about the CyrclePhone 4G?</h3>
+      <h3>How Can I Find Out More About The Cyrcle Phone 4G and dTOOR?</h3>
     </div>
     <div class="answer">
-      <p>Sign up for our updates! It's the best way to know about our current development. Otherwise, you can check out our Cyrcle Phone 2G Kickstarter and Social Purpose Statement!</p>
+      <p>Sign up for our updates! It's the best way to know about our current development. You can also check out The Cyrcle Phone 2G Kickstarter or dTOOR’s Annual Report which describes more about our Social Purpose Corporation. </p>
     </div>
   </div>
 </div>
